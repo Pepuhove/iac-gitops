de068bccb9cd826f58054019e0202cfcc41f2e32
pepu <pepshove@gmail.com>
2025-08-28 18:20:54 +0200

    iac

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,40 +1,46 @@
-# EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.1.5"
+  version = "~> 21.0"
 
-  name                   = var.name
-  kubernetes_version = var.k8s_version
+  name               = var.name
+  kubernetes_version = "1.33"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
+
+  # Optional
   endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
+  # control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
 
-  # create_cluster_security_group = false
-  create_node_security_group    = false
-
-  enable_cluster_creator_admin_permissions = true
-
-  addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
+  # EKS Managed Node Group(s)
   eks_managed_node_groups = {
-    initial = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      # ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.micro"]
-      min_size       = 2
-      max_size       = 4
-      desired_size   = 2
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
     }
   }
 
-  tags = var.tags
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
