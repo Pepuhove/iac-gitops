2b53e02fa9f677a23e48b28b4d8a03b2fa638cd8
pepu <pepshove@gmail.com>
2025-08-28 15:44:05 +0200

    iac

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,28 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-}
-
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "3.0.2"
-    }
-  }
-}
-
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.38.0"
-    }
-  }
 }
